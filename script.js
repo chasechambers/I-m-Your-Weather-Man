250e1212e8f102f5ffe2bd43380c4c43f9bd4395
Chase <chambers.chase@gmail.com>
2023-04-25 14:30:29 -0400

    Started basic HTML

--- script.js
+++ script.js
@@ -6,10 +6,5 @@ fetch('https://api.openweathermap.org/data/2.5/forecast?lat=36.0971&lon=-80.4192
         return res.json();
     })
         .then(data => {
-
-            let i = 0;
-            for(i = 0; i < data.length; i++);
-                console.log(data);
-                let weatherDetails = data.list [i];
-                document.querySelector('ul').insertAdjacentHTML('beforeend', weatherDetails);
+            console.log(data);
             });
