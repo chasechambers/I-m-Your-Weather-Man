54480d9562b3b9321a271551771e3688237db8ce
Chase <chambers.chase@gmail.com>
2023-04-25 15:22:34 -0400

    Fetch working

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // API KEY 947fa14763c9be5d3b12ac0ba332906f
 
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat=36.0971&lon=-80.4192&appid=947fa14763c9be5d3b12ac0ba332906f')
-    .then(res => {
-        return res.json();
+fetch('https://api.openweathermap.org/data/2.5/forecast?lat=36.0971&lon=-80.4192&units=imperial&appid=947fa14763c9be5d3b12ac0ba332906f')
+    .then((data) => {
+        return data.json()
     })
-        .then(data => {
-            console.log(data);
+        .then((completeddata) => {
+            console.log(completeddata.list[0].main.feels_like);
             });
