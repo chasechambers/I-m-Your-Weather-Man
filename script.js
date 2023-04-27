d0a9cc1addec9d09c13c0d4110744b1ea86eeb38
Chase <chambers.chase@gmail.com>
2023-04-27 16:30:24 -0400

    Final Product - Everything functioning

--- script.js
+++ script.js
@@ -35,28 +35,18 @@ let currentWeatherSection = document.getElementById('current-weather-section');
 let searchBox = document.getElementById('search-box');
 let searchButton = document.getElementById('search-button');
 const errorBox = document.getElementById('error-box');
-let buttonList = document.getElementById('button-list')
+let buttonList = document.getElementById('button-list');
+let buttonLinks = document.getElementById('buttonLinks');
+let fiveDaySection = document.getElementById('five-day-section');
 
 
-
-// const weatherURL = 'https://api.openweathermap.org/data/2.5/';
-// let query = 'q=';
-// let userUnits = 'imperial';
 const appID = '947fa14763c9be5d3b12ac0ba332906f';
 
 
-// const buildURL = (route) => {
-
-
-//     query = query + userCity;
-//     return weatherURL + route + '?' + query + '&units=' + userUnits + appID;
-// }
-
 // SEARCH BUTTON
 
-
-
 searchButton.addEventListener('click', function(e) {
+    console.log(e);
     let city = document.getElementById('city').value;
     let state = document.getElementById('state').value;
     let country = document.getElementById('country').value;
@@ -73,33 +63,35 @@ searchButton.addEventListener('click', function(e) {
     fetchWeather(userCity);
     displayCities(userCity);
     currentWeatherSection.style.display = "initial";
+    fiveDaySection.style.display = "initial";
 });
+
+// LOCAL STORAGE
 
 let saveCity = (userCity) => {
     let allCities = JSON.parse(localStorage.getItem('saved-cities')) || [];
     if(!allCities.includes(userCity)) {
-    allCities.push(userCity);
-    localStorage.setItem('saved-cities', JSON.stringify(allCities));
-}
+        allCities.unshift(userCity);
+        localStorage.setItem('saved-cities', JSON.stringify(allCities));
+    }
 };
+
+// HISTORY BUTTON CREATOR 
+
+buttonLinks.addEventListener('click', function(e) {
+    console.log(e.target.innerText);
+    fetchCity(e.target.innerText);
+    fetchWeather(e.target.innerText);
+});
 
 let displayCities = (allCities) => {
     allCities=JSON.parse(localStorage.getItem('saved-cities'));
-    console.log(allCities)
     let i = 0;
     for (i=0; i<allCities.length; i++) {
-        if (allCities.length > 8) {
-            allCities.length = 8;
-            allCities.shift();
-        }
         var li = document.createElement("li");
         let createCityButton = document.createElement('button');
         createCityButton.innerHTML = allCities[i];
-        createCityButton.addEventListener = ('click', function() {
-            fetchCity();
-            fetchWeather();
-            console.log('I work')
-        });
+        createCityButton.classList.add("btn-primary", "btn")
         li.appendChild(createCityButton);
         buttonList.appendChild(li);
     }
@@ -111,12 +103,13 @@ let clearButtons = () => {
       }
 }
 
+
+
 // THIS FETCHES THE 5 DAY FORECAST
 
 function fetchCity(userCity) {
     let urlQuery = `https://api.openweathermap.org/data/2.5/forecast?q=${userCity}&units=imperial&appid=${appID}`;
-    return fetch(urlQuery)
-        
+    return fetch(urlQuery)       
         .then((response) => {
             if (response.ok) {
                 return response.json();
@@ -162,7 +155,7 @@ function fetchCity(userCity) {
 
 // THIS FETCHES THE CURRENT WEATHER
 
-function fetchWeather() {
+function fetchWeather(userCity) {
     let urlFetchWeather = `https://api.openweathermap.org/data/2.5/weather?q=${userCity}&units=imperial&appid=${appID}`
     return fetch(urlFetchWeather)
         .then((response) => {
@@ -177,7 +170,6 @@ function fetchWeather() {
             }
         })
         .then((completedata) => {
-            console.log(completedata);
                 currentCityDay.textContent = completedata.name + ' ' + dayjs(completedata.dt*1000).format('MM-DD-YYYY');
                 currentCityIcon.src = `http://openweathermap.org/img/w/${completedata.weather[0].icon}.png`;
                 currentTemp.textContent = 'Temp: ' + completedata.main.temp + ' °F';
@@ -187,5 +179,5 @@ function fetchWeather() {
         .catch((err) => {
             errorBox.textContent = err;
         });
-}
+};
 
